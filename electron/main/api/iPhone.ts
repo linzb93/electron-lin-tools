46a8e0b04a3590999b40e12583aa672f9ccdb3dc
林志斌 <linzb93@qq.com>
2024-04-07 16:56:37 +0800

    iPhone相关的代码

--- electron/main/api/iPhone.ts
+++ electron/main/api/iPhone.ts
@@ -1,9 +1,43 @@
+import express from 'express';
+import {Notification, clipboard} from 'electron';
 import Controller from "../plugins/route/Controller";
 import { Route } from "../plugins/route/decorators";
 import { HTTP_STATUS } from "../plugins/constant";
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
 export default class extends Controller {
+  private app;
   constructor() {
     super();
+    const app = express();
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json({ limit: '5mb' }));
+    app.use(cors());
+    
+    // iPhone发往电脑
+    app.post('/copy', (req, res) => {
+      const {text} = req.body;
+      const notice = new Notification({
+        title: '温馨提醒',
+        body: '收到来自iPhone的剪贴'
+      });
+      notice.show();
+      clipboard.writeText(decodeURIComponent(text));
+      res.send('ok');
+    });
+
+    // iPhone从电脑获取
+    app.get('/copy-data', (req, res) => {
+      const copyData = clipboard.readText();
+      res.send(copyData);
+    });
+
+    // iPhone批量获取电脑图片
+    app.get('/getImgList', async(req, res) => {});
+    // iPhone批量给电脑发送图片
+    app.post('/sendImg', async (req, res) => {});
+    app.listen(5010);
+    this.app = app;
   }
 }
